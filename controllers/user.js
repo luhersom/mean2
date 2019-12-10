51b9d10d68fef645f9e23bb986b9e3a8e888bfb2
luhersom <58355100+luhersom@users.noreply.github.com>
2019-12-09 21:05:54 -0500

    login

--- controllers/user.js
+++ controllers/user.js
@@ -3,11 +3,6 @@ var dbmysql = require('../routes/dbmysql');
 var bcrypt = require('bcrypt-nodejs');
 var User = require('../models/user');
 
-function pruebas(req, res){
-	res.status(200).send({
-		message: 'Probando una accion'
-	});
-}
 
 function saveUser(req, res){
 	var user = new User();
@@ -19,7 +14,7 @@ function saveUser(req, res){
 	user.name = params.name;
 	user.surname = params.surname;
 	user.email = params.email;
-	user.role = 'ROLE_USER';
+	user.role = 'ROLE_ADMIN';
 	user.image = 'null';
 
 	if (params.password) {
@@ -66,7 +61,89 @@ function saveUser(req, res){
 
 }
 
+function loginUser(req, res){
+	var params = req.body;
+
+	var email = params.email;
+	var password = params.password;
+
+	User.findOne({email: email.toLowerCase()}, (err, user) =>{
+		if (err){
+			res.status(500).send({message: 'Error en la peticion'});
+		}else{
+			if(!user){
+				res.status(404).send({message: 'El usuario no existe'});
+			}else{
+				//comprobar la contraseña
+				bcrypt.compare(password, user.password, function(err, check){
+					if(check){
+						//devolver datos de usuario logueado
+						if(params.gethash){
+							//devolver un token de jwt
+						}else{
+							res.status(200).send({user});
+						}
+					}else{
+						res.status(404).send({message: 'El usuario no ha podido loguearse'});
+					}
+				});
+			}
+			
+		}
+	});
+}
+
+function loginUserMySql(req, res){
+	var params = req.body;
+
+	var email = params.email;
+	var password = params.password;
+
+	var contra ='';
+
+	console.log(params)
+
+	// Inicio - Insert en Base de Datos MySQL
+	var sql = "select email from users where email = ?";
+		dbmysql.query(sql, [email], function (err, user) {
+			if (err){	
+				res.status(500).send({message: 'Error en la peticion'});
+			}else{
+				Object.keys(user).forEach(function(key) {
+					var row = user[key];
+					contra = row.email
+				});	
+				if(contra !== email){					
+					res.status(404).send({message: 'El usuario no existe'});
+				}else{
+				//comprobar la contraseña
+					var sql = "select * from users where email = ?";
+					dbmysql.query(sql, [email], function (err, result, fields) {
+						if (err) throw err;
+						Object.keys(result).forEach(function(key) {
+							var row = result[key];
+							bcrypt.compare(password, row.password, function(err, check){
+								if(check){
+										//devolver datos de usuario logueado
+										if(params.gethash){
+											//devolver un token de jwt
+										}else{
+											res.status(200).send({user});
+										}
+								}else{
+									res.status(404).send({message: 'El usuario no ha podido loguearse'});
+								}
+							});
+						});		
+				    });
+				}	
+			}	
+		});
+}
+
 module.exports =  {
-	pruebas,
-	saveUser
+	saveUser,
+	loginUser,
+	loginUserMySql
 }; 
+
--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,9 @@ var  UserController = require('../controllers/user');
 
 var api = express.Router();
 
-api.get('/probando-controlador', UserController.pruebas);
+
 api.post('/register', UserController.saveUser);
+api.post('/login', UserController.loginUser);
+api.post('/login-mysql', UserController.loginUserMySql);
 
 module.exports = api;
